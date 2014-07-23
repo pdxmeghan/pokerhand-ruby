05da9ad836a02e68259c7d89e266d11b5ca9f48d
Meghan Lindsley & Carolyn Huynh <student@epicodus.com>
2014-07-23 15:59:28 -0700

    All specs pass

--- lib/pokerhand.rb
+++ lib/pokerhand.rb
@@ -1,19 +1,49 @@
 def pokerhand(cards)
   hand_array = cards
-  num_array = [hand_array[0][0], hand_array[1][0], hand_array[2][0], hand_array[3][0], hand_array[4][0]]
-  suit_array = [hand_array[0][1], hand_array[1][1], hand_array[2][1], hand_array[3][1], hand_array[4][1]]
-  sort = num_array.sort
-  pull = sort.uniq
+  num_array = []
+  suit_array = []
+  card_value = {
+                'J' => 11,
+                'Q' => 12,
+                'K' => 13,
+                'A' => 14}
+
+  hand_array.each do |card|
+    if(card.length == 3)
+      num_array << card[0] + card[1]
+      suit_array << card[2]
+    else
+      if (card[0].to_i != 0)
+        num_array << card[0]
+        suit_array << card[1]
+      else
+        num_array << card_value[card[0]]
+        suit_array << card[1]
+      end
+    end
+  end
+
+sorted_num_array = []
+  num_array.each do |i|
+    sorted_num_array << i.to_i
+  end
+
+  sorted = sorted_num_array.sort
   results = []
 
   doubles = Hash.new(0)
+  suit_hash = Hash.new(0)
 
   num_array.each do |element|
     doubles[element] += 1
   end
 
-  if((sort[4].to_i) - (sort[0].to_i) + 1) == 5
-    results << "straight"
+  suit_array.each do |element|
+    suit_hash[element] += 1
+  end
+
+  if doubles.length == 2 && doubles.has_value?(3)
+    results << "full house"
   elsif doubles.length == 3 && doubles.has_value?(3)
     results << "three of a kind"
   elsif doubles.length == 4 && doubles.has_value?(2)
@@ -22,8 +52,17 @@ def pokerhand(cards)
     results << "four of a kind"
   elsif doubles.length == 3 && doubles.has_value?(2)
     results << "two pair"
+  elsif (((sorted[4].to_i) - (sorted[0].to_i) + 1) == 5) && (suit_hash.length == 1 && suit_hash.has_value?(5))
+    results << "straight flush"
+  elsif((sorted[4].to_i) - (sorted[0].to_i) + 1) == 5
+    results << "straight"
+  elsif (suit_hash.length == 1)
+    results << "flush"
   end
 
 results.join("")
 
 end
+
+# p pokerhand(["JC", "10C", "9C", "8C", "7C"])
+
--- spec/pokerhand_spec.rb
+++ spec/pokerhand_spec.rb
@@ -17,4 +17,19 @@ describe('pokerhand') do
   it("returns 'two pair'") do
     pokerhand(['2S', '2S', '4D', '5C', '5S']).should(eq('two pair'))
   end
+  it("returns 'two pair'") do
+    pokerhand(["10D", "10S", "2S", "2D", "4H"]).should(eq('two pair'))
+  end
+  it("returns 'two of a kind' for a card with ten") do
+    pokerhand(["AS", "10S", "AH", "JH", "QD"]).should(eq('two of a kind'))
+  end
+  it("returns 'straight flush'") do
+    pokerhand(["JC", "10C", "9C", "8C", "7C"]).should(eq('straight flush'))
+  end
+  it("returns 'full house'") do
+    pokerhand(["5S", "5H", "5D", "8S", "8H"]).should(eq('full house'))
+  end
+  it("returns 'flush'") do
+    pokerhand(["AS", "JS", "10S", "6S", "7S"]).should(eq('flush'))
+  end
 end
