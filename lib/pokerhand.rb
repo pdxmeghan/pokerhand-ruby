990f8442aa9083b53c3a56e38a23fa3581890ed9
Meghan Lindsley & Carolyn Huynh <student@epicodus.com>
2014-07-23 11:12:36 -0700

    Five specs have passed, five more to go

--- lib/pokerhand.rb
+++ lib/pokerhand.rb
@@ -3,10 +3,25 @@ def pokerhand(cards)
   num_array = [hand_array[0][0], hand_array[1][0], hand_array[2][0], hand_array[3][0], hand_array[4][0]]
   suit_array = [hand_array[0][1], hand_array[1][1], hand_array[2][1], hand_array[3][1], hand_array[4][1]]
   sort = num_array.sort
+  pull = sort.uniq
   results = []
+
+  doubles = Hash.new(0)
+
+  num_array.each do |element|
+    doubles[element] += 1
+  end
 
   if((sort[4].to_i) - (sort[0].to_i) + 1) == 5
     results << "straight"
+  elsif doubles.length == 3 && doubles.has_value?(3)
+    results << "three of a kind"
+  elsif doubles.length == 4 && doubles.has_value?(2)
+    results << "two of a kind"
+  elsif doubles.length == 2 && doubles.has_value?(4)
+    results << "four of a kind"
+  elsif doubles.length == 3 && doubles.has_value?(2)
+    results << "two pair"
   end
 
 results.join("")
--- spec/pokerhand_spec.rb
+++ spec/pokerhand_spec.rb
@@ -5,7 +5,16 @@ describe('pokerhand') do
   it("returns a 'straight'") do
     pokerhand(['2S', '3C', '4D', '5C', '6H']).should(eq('straight'))
   end
-  it("returns a 'pair'") do
-    pokerhand(['2S', '3C', '4D', '5C', '2H']).should(eq('pair'))
+  it("returns 'two of a kind'") do
+    pokerhand(['2S', '3C', '4D', '5C', '2H']).should(eq('two of a kind'))
+  end
+  it("returns 'three of a kind'") do
+    pokerhand(['2S', '4D', '5H', '2C', '2H']).should(eq('three of a kind'))
+  end
+  it("returns 'four of a kind'") do
+    pokerhand(['2S', '2D', '2C', '2H', '4D']).should(eq('four of a kind'))
+  end
+  it("returns 'two pair'") do
+    pokerhand(['2S', '2S', '4D', '5C', '5S']).should(eq('two pair'))
   end
 end
